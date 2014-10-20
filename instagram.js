31bad1e36eef9d1a5fccf23651518b487d16d1f8
fredrikdegis <alfbertil@bertildmansmbp2.dhcp.sthlm.hyperisland.local>
2014-10-20 11:47:55 +0200

    första versionen instagram .js

---
+++ instagram.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="path/to/instafeed.min.js"></script>
+
+<script type="text/javascript">
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: 'YOUR_CLIENT_ID'
+    });
+    feed.run();
+</script>
